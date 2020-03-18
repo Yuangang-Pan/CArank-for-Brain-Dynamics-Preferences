7f90af4351096cf67f653fd53847cb89a709f199
Yuangang pan <suiyuan4325@gmail.com>
2020-03-18 17:52:39 +1100

    Update NN_Regression.py

--- CArank/NN_Regression.py
+++ CArank/NN_Regression.py
@@ -15,6 +15,10 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import scipy.io
 import os
+SEED = 9159
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
 torch.backends.cudnn.deterministic = True
 os. environ['CUDA_VISIBLE_DEVICES'] = '1'
 flag_cuda = torch.cuda.is_available()
